8b37c23a1d48b14381e19be6fc864a3b3a27a9a3
Vasyl Nabochenko <vnaboc@softserveinc.com>
2025-08-08 22:45:40 +0300

    feat: add Identity server 8 support with sample

---
+++ samples/4_IdentityServer8/AspNet.IdentityServer8/Config.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using IdentityServer8.Models;
+
+namespace NetDevPack.Security.Jwt.IdentityServer8.Tests
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new IdentityResource[]
+            {
+                new IdentityResources.OpenId()
+            };
+        }
+
+        public static IEnumerable<ApiResource> GetApis()
+        {
+            return new ApiResource[] { };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new Client[] { };
+        }
+    }
+}
